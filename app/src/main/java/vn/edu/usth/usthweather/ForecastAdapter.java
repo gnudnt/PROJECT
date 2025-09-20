2cbd3e8275b6b7fb0f1bdc9a731d1f0856f23f93
gnudnt <ntd150404@gmail.com>
2025-09-20 22:17:29 +0700

    Push full project with all changes

--- app/src/main/java/vn/edu/usth/usthweather/ForecastAdapter.java
+++ app/src/main/java/vn/edu/usth/usthweather/ForecastAdapter.java
@@ -1,53 +1,53 @@
-package vn.edu.usth.usthweather;
+    package vn.edu.usth.usthweather;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+    import android.view.LayoutInflater;
+    import android.view.View;
+    import android.view.ViewGroup;
+    import android.widget.ImageView;
+    import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+    import androidx.annotation.NonNull;
+    import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
+    import java.util.List;
 
-public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ViewHolder> {
+    public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ViewHolder> {
 
-    private List<ForecastItem> items;
+        private List<ForecastItem> items;
 
-    public ForecastAdapter(List<ForecastItem> items) { this.items = items; }
+        public ForecastAdapter(List<ForecastItem> items) { this.items = items; }
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_forecast, parent, false);
-        return new ViewHolder(v);
-    }
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_forecast, parent, false);
+            return new ViewHolder(v);
+        }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        ForecastItem item = items.get(position);
-        holder.day.setText(item.day);
-        holder.comment.setText(item.comment);
-        holder.temp.setText(item.temp);
-        holder.comment.setText(item.comment);
-        holder.icon.setImageResource(item.iconRes);
-    }
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            ForecastItem item = items.get(position);
+            holder.day.setText(item.day);
+            holder.comment.setText(item.comment);
+            holder.temp.setText(item.temp);
+            holder.comment.setText(item.comment);
+            holder.icon.setImageResource(item.iconRes);
+        }
 
-    @Override
-    public int getItemCount() { return items.size(); }
+        @Override
+        public int getItemCount() { return items.size(); }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView day, comment, temp;
-        ImageView icon;
+        static class ViewHolder extends RecyclerView.ViewHolder {
+            TextView day, comment, temp;
+            ImageView icon;
 
-        ViewHolder(View v) {
-            super(v);
-            day = v.findViewById(R.id.item_day);
-            comment = v.findViewById(R.id.item_comment);
-            temp = v.findViewById(R.id.item_temp);
-            icon = v.findViewById(R.id.item_icon);
+            ViewHolder(View v) {
+                super(v);
+                day = v.findViewById(R.id.item_day);
+                comment = v.findViewById(R.id.item_comment);
+                temp = v.findViewById(R.id.item_temp);
+                icon = v.findViewById(R.id.item_icon);
+            }
         }
     }
-}
